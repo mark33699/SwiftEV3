44f2a3efe85845aacc522f27b8916f096a27a4f3
mark <mark.appledeveloper@gmail.com>
2019-06-21 01:17:50 +0800

    好吧先這樣

--- SwiftEV3/LMEV3ConnectManager.swift
+++ SwiftEV3/LMEV3ConnectManager.swift
@@ -6,6 +6,12 @@
 //  Copyright © 2019 謝飛飛. All rights reserved.
 //
 
+/*
+ 2019-06-21 01:07:42.767500+0800 SwiftEV3[3253:563781] [MC] System group container for systemgroup.com.apple.configurationprofiles path is /private/var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles
+ 2019-06-21 01:07:42.769359+0800 SwiftEV3[3253:563781] [MC] Reading from public effective user settings.
+ 2019-06-21 01:07:42.824258+0800 SwiftEV3[3253:563781] A constraint factory method was passed a nil layout anchor.  This is not allowed, and may cause confusing exceptions. Break on BOOL _NSLayoutConstraintToNilAnchor(void) to debug.  This will be logged only once.  This may break in the future.
+ */
+
 import Foundation
 import ExternalAccessory
 
@@ -24,7 +30,7 @@ class LMEV3ConnectManager //LM = LEGO mindstorms
     }
     
     //被動
-    @objc func accessoryConnected(notif: Notification) //只有第一次Alert才會call, 無法復現
+    @objc func accessoryConnected(notif: Notification) //只有第一次Alert才會call, 無法復現 (好像EV3重開就會了)
     {
         print("已經連上休士頓了")
 //        if let accessory = notif.userInfo?[EAAccessoryKey] as? EAAccessory
@@ -45,24 +51,6 @@ class LMEV3ConnectManager //LM = LEGO mindstorms
     func findEV3Accessory()
     {
         let manager = EAAccessoryManager.shared()
-        
-//        manager.showBluetoothAccessoryPicker(withNameFilter: nil)
-//        { (error) in
-//
-//            if error != nil
-//            {
-//                print("到底怎樣啦\(error.debugDescription)")
-//            }
-        
-//            if let accessory = manager.connectedAccessories.first
-//            {
-//                if Ev3Connection.supportsEv3Protocol(accessory: accessory)
-//                {
-//                    self.connectEV3(accessory)
-//                }
-//            }
-//        }
-        
         let connecteds = manager.connectedAccessories
         if connecteds.count > 0 //只要Picker曾經選過, 這邊就會有
         {
@@ -74,13 +62,32 @@ class LMEV3ConnectManager //LM = LEGO mindstorms
                 }
             }
         }
+        else
+        {
+            manager.showBluetoothAccessoryPicker(withNameFilter: nil)
+            { (error) in
+    
+                if error != nil
+                {
+                    print("選擇EV3時錯誤：\(error.debugDescription)")
+                }
+    
+                if let accessory = manager.connectedAccessories.first
+                {
+                    if Ev3Connection.supportsEv3Protocol(accessory: accessory)
+                    {
+                        self.connectEV3(accessory)
+                    }
+                }
+            }
+        }
     }
     
     fileprivate func connectEV3(_ accessory: EAAccessory)
     {
         let connection = Ev3Connection.init(accessory: accessory)
         connection.open()
-//        brick = Ev3Brick.init(connection: connection)
-//        print("\(brick.debugDescription)")
+        brick = Ev3Brick.init(connection: connection) //經測試, 好像原因是不生brick就有一定機率閃退
+        print("\(brick.debugDescription)")
     }
 }
